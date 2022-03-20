29f03314a31e101c7436ad5490b0f6736bdc87da
Marcel Homšak <38257325+marcelhomsak@users.noreply.github.com>
2022-03-20 14:50:50 +0100

    Optimized time efficiency by making a second for loop in most_optimal() function dynamically long. At the beginning when there are fewer hops, possibilities for the shortest path are bigger, and with more hops we change for loop length exponentially. Given function was calculated. Optimized accuracy for path by generating new next location if we were already at a specific location. If next location was already visited and if set of locations where we can go but haven't gone there yet is not empty, then we can go to new location else we need to go back.

--- optimal_path.py
+++ optimal_path.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from random import choice
+from math import e
 from time import time
 
 from connections import connections
@@ -12,9 +13,11 @@ for k1, k2 in connections:
     s[k2].update({k1})
 
 ####################
-from_point = "Piran"
-to_point = "Gornja Radgona"
+from_point = "Krško"
+to_point = "Piran"
 max_hop = 15
+base = 4
+power = 0.75
 ####################
 
 def path(from_point, to_point, max_hop_var):
@@ -23,15 +26,16 @@ def path(from_point, to_point, max_hop_var):
         if len(path_list) > max_hop_var:
             break
         rnd_next = choice(list(s[from_point]))
-        while rnd_next in path_list and not choice(list(s[from_point])) in path_list:
-            rnd_next = choice(list(s[from_point]))
+        was_not_there = s[from_point].difference(set(path_list))
+        if rnd_next in path_list and was_not_there != set():
+            rnd_next = choice(list(was_not_there))
         from_point = rnd_next
         path_list.append(from_point)
     return path_list
 
 def most_optimal():
     for max_hop_var in range(max_hop + 1):
-        for _ in range(1000):
+        for _ in range(int(base*e**(power*max_hop_var))):
             path_list = path(from_point, to_point, max_hop_var)
             if len(path_list) == max_hop_var:
                 return f"From: {from_point}\nTo: {to_point}\nPath: {path_list}\nNumber of hops: {max_hop_var}"
